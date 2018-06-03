73920db85da306b193da72b310ae604577e6c3a5
Raeef Refai <raeef.refai@gmail.com>
2018-06-03 21:05:35 +0300

    add react resolver

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import SwaggerParser from 'swagger-parser';
+
 import { async } from 'react-redux';
+import { resolve } from 'react-resolver';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Home from './containers/Home';
 
+@resolve('openapi', () => {
+
+})
 class Routes extends Component {
   render() {
     return (
